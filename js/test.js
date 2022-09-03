0186121b99ad0bd3f86c419895641c0ef358f787
Rakibul Hasan <39860864+rakibhasantusar@users.noreply.github.com>
2022-09-03 19:26:25 +0600

    footer done

--- js/test.js
+++
@@ -1,5 +0,0 @@
-let data = [{ "id": 1, "is_read": true, }, { "id": 2, "is_read": true, }, { "id": 3, "is_read": false, }, { "id": 4, "is_read": true, },],
-    length = data.filter(function (item) {
-        return item.is_read;
-    }).length;
-console.log(length)
