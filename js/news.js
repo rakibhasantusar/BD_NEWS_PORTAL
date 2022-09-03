abea14641db8a4c119d473484c0834632cf92866
Rakibul Hasan <39860864+rakibhasantusar@users.noreply.github.com>
2022-09-03 13:16:02 +0600

    new card style add

--- js/news.js
+++ js/news.js
@@ -26,25 +26,29 @@ const loadDisplayNews = async (id) => {
 
 const displayNews = news => {
     const newsDetailsBar = document.getElementById('news-detail');
+    newsDetailsBar.innerHTML = '';
     news.forEach(newsDetail => {
-        console.log(newsDetail);
-        const { title, image_url, details, author, total_view } = newsDetail
+        // console.log(newsDetail);
+        const { title, thumbnail_url, image_url, details, author, total_view, _id } = newsDetail
         const newsDiv = document.createElement('div');
-        newsDiv.classList.add('col')
         newsDiv.innerHTML = `
-        <div class="card h-100">
-        <img src="${image_url}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${title}</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                to additional content. This content is a little bit longer.</p>
-            <div class="d-flex justify-content-around">
-                <div class="d-flex justify-content-around w-75">
-                    <img class="img-fluid w-25 rounded-circle" src="${author.img}" alt="">
-                    <p class="ml-2" >Author:${author.name}</p>
-                </div>
-                <div class="">
-                    <p>Views: ${total_view}</p>
+        <div onclick="displayNewsModal('${_id}')" class="card h-100 m-4 mx-auto" data-bs-toggle="modal"
+        data-bs-target="#exampleModal" class="card mb-3" style="max-width: 740px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${thumbnail_url}" class="img-fluid w-100 rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text">${details.slice(0, 200)}...</p>
+                    <div class="d-flex justify-content-evenly  align-items-center ">
+                        <img class="img-fluid w-25 p-4 rounded-circle" src="${author.img}" alt="">
+                        <div class="d-flex justify-content-around">
+                        <p class="">Author: <span class="text-primary"> ${author.name ? author.name : 'no data available'} </span></p>&nbsp;&nbsp;
+                        <p class="">Views:<span class="text-primary"> ${total_view ? total_view : 'no views'}</span></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -53,6 +57,45 @@ const displayNews = news => {
         newsDetailsBar.appendChild(newsDiv)
     });
 }
+const displayNewsModal = async (news_id) => {
+    const url = ` https://openapi.programming-hero.com/api/news/${news_id}`
+    const response = await fetch(url);
+    const data = await response.json();
+    displayNewsModalDetail(data.data[0]);
+}
+displayNewsModal()
+
+const displayNewsModalDetail = newsModal => {
+    console.log(newsModal);
+    const newsDetailsBarModal = document.getElementById('news-detail-modal');
+    newsDetailsBarModal.innerHTML = '';
+    const { title, image_url, details, author, total_view, rating } = newsModal
+    const newsDiv = document.createElement('div');
+    newsDiv.classList.add('col')
+    newsDiv.innerHTML = `
+        <div class="card h-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <img src="${image_url}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p>${details.slice(0, 200)} ...</p>
+            <div class="d-flex justify-content-around bg-dark ">
+                <p class="text-light my-auto">Engage: <span class="text-info">${rating.badge}</span></p>
+                <p class="text-light my-auto" >Rating: <span class="text-info">${rating.number}</span></p>
+            </div>
+            <div class="d-flex justify-content-evenly  align-items-center ">
+                <img class="img-fluid w-25 p-4 rounded-circle" src="${author.img}" alt="">
+                <div class="d-flex justify-content-around">
+                    <p class="">Author: <span class="text-primary"> ${author.name ? author.name : 'no data available'} </span></p>&nbsp;&nbsp;
+                    <p class="">Views:<span class="text-primary"> ${total_view ? total_view : 'no views'}</span></p>
+                </div>
+        </div>
+        </div>
+    </div>
+        `;
+    newsDetailsBarModal.appendChild(newsDiv)
+}
+
+
 
 
 setAllCategories()
